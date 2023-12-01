178cf15980892d79a61611e69896dfc3e8bcce48
Vizel Leonid <spartanlaska@gmail.com>
2023-12-01 18:03:17 +0300

    Добавьте файлы проекта.

---
+++ FizraDnevnikWeb/Exporter.cs
@@ -0,0 +1,110 @@
+﻿using ClosedXML.Excel;
+using FizraDnevnikWeb.Pages;
+
+namespace FizraDnevnikWeb;
+
+public sealed class Exporter
+{
+    public byte[] Export(IndexModel model)
+    {
+        using (XLWorkbook book = new XLWorkbook("pattern.xlsx"))
+        {
+            var sheet = book.Worksheet(3);
+            if (model.AA17 != null)
+            {
+                sheet.Cell(17, 27).Value = model.AA17;
+            }
+            if (model.Group != null)
+            {
+                sheet.Cell(19, 27).Value = model.Group;
+            }
+
+            if (model.Height != null)
+            {
+                sheet.Cell(9, 78).Value = model.Height;
+            }
+            if (model.Weight != null)
+            {
+                sheet.Cell(12, 78).Value = model.Weight;
+            }
+            if (model.FatPercent != null)
+            {
+                sheet.Cell(15, 78).Value = model.FatPercent;
+            }
+            if (model.MusclePercent != null)
+            {
+                sheet.Cell(18, 78).Value = model.MusclePercent;
+            }
+            if (model.IMT != null)
+            {
+                sheet.Cell(21, 78).Value = model.IMT;
+            }
+
+            if (model.PulsePressureDate != null)
+            {
+                sheet.Cell(124, 50).Value = model.PulsePressureDate.Value.ToString("dd.MM.yyyy");
+            }
+            if (model.Pressure != null)
+            {
+                sheet.Cell(132, 50).Value = model.Pressure;
+            }
+            if (model.Pulse != null)
+            {
+                sheet.Cell(138, 50).Value = model.Pulse;
+            }
+
+            if (model.OrtostatDate != null)
+            {
+                sheet.Cell(36, 50).Value = model.OrtostatDate.Value.ToString("dd.MM.yyyy");
+            }
+            if (model.OrtostatLayingPulse != null)
+            {
+                sheet.Cell(40, 50).Value = model.OrtostatLayingPulse;
+            }
+            if (model.OrtostatStandingPulse != null)
+            {
+                sheet.Cell(44, 50).Value = model.OrtostatStandingPulse;
+            }
+            if (model.OrtostatResult != null)
+            {
+                sheet.Cell(48, 50).Value = model.OrtostatResult;
+            }
+
+            if (model.RuffieDate != null)
+            {
+                sheet.Cell(68, 9).Value = model.RuffieDate.Value.ToString("dd.MM.yyyy");
+            }
+            if (model.RuffieBefore != null)
+            {
+                sheet.Cell(71, 9).Value = model.RuffieBefore;
+            }
+            if (model.RuffieAfter != null)
+            {
+                sheet.Cell(74, 9).Value = model.RuffieAfter;
+            }
+            if (model.RuffieRelax != null)
+            {
+                sheet.Cell(77, 9).Value = model.RuffieRelax;
+            }
+
+            if (model.RuffieBefore != null)
+            {
+                sheet.Cell(160, 73).Value = model.RuffieBefore;
+            }
+            if (model.RuffieAfter != null)
+            {
+                sheet.Cell(165, 73).Value = model.RuffieAfter;
+            }
+            if (model.RuffieRelax != null)
+            {
+                sheet.Cell(170, 73).Value = model.RuffieRelax;
+            }
+
+            using (MemoryStream memStream = new MemoryStream())
+            {
+                book.SaveAs(memStream);
+                return memStream.ToArray();
+            }
+        }
+    }
+}
---
+++ FizraDnevnikWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,111 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Weasel.Export.AspNetCore;
+
+namespace FizraDnevnikWeb.Pages;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Ячейка АА17")]
+    public string? AA17 { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Номер группы")]
+    public string? Group { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Рост (см)")]
+    public string? Height { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Вес (кг)")]
+    public string? Weight { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("% жировой ткани")]
+    public string? FatPercent { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("% мышечной ткани")]
+    public string? MusclePercent { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Индекс массы тела (IMT)")]
+    public string? IMT { get; set; }
+
+    [BindProperty]
+    [DisplayName("Дата")]
+    public DateOnly? OrtostatDate { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Пульс лежа за 1 мин")]
+    public string? OrtostatLayingPulse { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Пульс стоя за 1 мин")]
+    public string? OrtostatStandingPulse { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Результат (разница между пульсом стоя и пульсом лежа)")]
+    public string? OrtostatResult { get; set; }
+
+    [BindProperty]
+    [DisplayName("Дата")]
+    public DateOnly? RuffieDate { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Пульс перед нагрузкой (за 15 сек)")]
+    public string? RuffieBefore { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Пульс после нагрузки (за 15 секунд)")]
+    public string? RuffieAfter { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Пульс за последние 15 сек в первые минуты отдыха")]
+    public string? RuffieRelax { get; set; }
+
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Поднимания туловища из положения лежа (кол-во повторов)")]
+    public string? Norma1 { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Отжимания (кол-во повторов)")]
+    public string? Norma2 { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Гибкость (см)")]
+    public string? Norma3 { get; set; }
+
+    [BindProperty]
+    [DisplayName("Дата")]
+    public DateOnly? PulsePressureDate { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Пульс")]
+    public string? Pulse { get; set; }
+    [BindProperty]
+    [MaxLength(200, ErrorMessage = "Максимальная длина - {0} символов!")]
+    [DisplayName("Артериальное давление")]
+    public string? Pressure { get; set; }
+
+    public Task<IActionResult> OnPost()
+    {
+        IActionResult result;
+        if (!ModelState.IsValid)
+        {
+            result = Page(); 
+        }
+        else
+        {
+            var exporter = new Exporter();
+            result = this.XlFile(exporter.Export(this), $"Дневник самоконтроля ({AA17}, {Group})");
+        }
+        return Task.FromResult(result);
+    }
+}
